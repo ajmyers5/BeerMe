f239f7d22bcc188ef126d3fa990bc46e7fc20114
John Franke <johnrfrankejr@gmail.com>
2020-01-21 20:18:15 -0600

    added granim code to index.html script file and style sheet

--- javascriptP1.js
+++ javascriptP1.js
@@ -1,3 +1,18 @@
+//adding the gradient background
+
+var granimInstance = new Granim({
+  element: '#canvas-basic',
+  direction: 'top-bottom',
+  isPausedWhenNotInView: true,
+  states : {
+      "default-state": {
+          gradients: [
+              ['#FCE181', '#E0B416'],
+          ]
+      }
+  }
+});
+
 let cityName = $(".city")
 
 // Here we are building the URL we need to query the database
