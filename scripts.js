c2228a6f5b944aff439f8d19ae401bd893742c1d
Kellen Presta <kpresta04@gmail.com>
2020-01-24 10:52:27 -0600

    added map on starting position

--- scripts.js
+++ scripts.js
@@ -1,35 +1,44 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  //adding event listeners to form and button
 
-    //adding event listeners to form and button
+  $("#findBtn").on("click", citySearch);
+  $("#searchForm").on("submit", citySearch);
 
-    $("#findBtn").on("click", citySearch)
-    $("#searchForm").on("submit", citySearch)
+  function citySearch(e) {
+    e.preventDefault();
 
-    function citySearch(e) {
-        e.preventDefault()
+    if ($("#cityInput").val() !== "") {
+      let city = $("#cityInput").val();
 
-        let city = $("#cityInput").val()
+      const queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=VBSRR4N0G21AGXXAQBIVHVETVMY5EMFV20R2AFBIENJKXHR2&client_secret=CI51EWKVLWPWG4YXIT1LR5OOKWDJDM3OLQVJBZRPC0QPCD0V&v=20180323&limit=10&near=${city}&query=brewery`;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        //these are coords for creating map
+        let lat = response.response.geocode.center.lat;
+        let long = response.response.geocode.center.lng;
+        console.log(lat);
+        console.log(long);
 
-        const queryURL =
-            `https://api.foursquare.com/v2/venues/explore?client_id=VBSRR4N0G21AGXXAQBIVHVETVMY5EMFV20R2AFBIENJKXHR2&client_secret=CI51EWKVLWPWG4YXIT1LR5OOKWDJDM3OLQVJBZRPC0QPCD0V&v=20180323&near=${city}&query=brewery`;
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
+        //explore endpoint gets a list of recommended breweries in the search area
 
-            //these are coords for creating map
-            console.log("Lat: " + response.response.geocode.center.lat)
-            console.log("Long: " + response.response.geocode.center.lng)
-
-            //explore endpoint gets a list of recommended breweries in the search area
-
-            let venueArray = response.response.groups[0].items;
-            //prints info of each brewery
-            venueArray.forEach(function (venue) {
-                console.log(venue.venue);
-            });
+        let venueArray = response.response.groups[0].items;
+        //prints info of each brewery
+        venueArray.forEach(function(venue) {
+          console.log(venue.venue);
         });
+        $("#map").empty(); // empty map div before we add a new one
+        mapboxgl.accessToken =
+          "pk.eyJ1Ijoia3A0MDUiLCJhIjoiY2s1cXl0emlzMDdvbDNtb250bXVveWFmcyJ9.JsFjY-BuUw2XW1CYWrZVVw";
+        var map = new mapboxgl.Map({
+          container: "map",
+          style: "mapbox://styles/mapbox/streets-v11",
+          center: [long, lat], // starting position
+          zoom: 12 // starting zoom
+        });
+        map.addControl(new mapboxgl.NavigationControl()); //add controls
+      });
     }
-
-
-})
+  }
+});
