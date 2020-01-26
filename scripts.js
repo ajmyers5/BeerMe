046520ee2893acd3c7fbed5c2b70a496d47d059d
Kellen Presta <kpresta04@gmail.com>
2020-01-26 17:14:40 -0600

    added click event for mobile

--- scripts.js
+++ scripts.js
@@ -169,9 +169,30 @@ $(document).ready(function() {
         //   popup.remove();
         // });
 
-        map.on("click", "places", function() {
-          map.getCanvas().style.cursor = "";
-          popup.remove();
+        // map.on("click", "places", function() {
+        //   map.getCanvas().style.cursor = "";
+        //   popup.remove();
+        // });
+        map.on("click", "places", function(e) {
+          // Change the cursor style as a UI indicator.
+          map.getCanvas().style.cursor = "pointer";
+
+          var coordinates = e.features[0].geometry.coordinates.slice();
+          var description = e.features[0].properties.description;
+
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+
+          // Populate the popup and set its coordinates
+          // based on the feature found.
+          popup
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
         });
       });
     }
